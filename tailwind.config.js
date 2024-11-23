7e3789a4c04b51b99e987b9df4b10a0e89bb3889
arifprog <arifprog1@gmail.com>
2024-11-24 03:17:02 +0600

    Add keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
     extend: {
       "fontFamily":{
         'jakarta':['"Plus Jakarta Sans", sans-serif'],
-      }
-    },
+      },
+      keyframes: {
+        fadeInOut: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 1 },
+          '100%': { opacity: 0 }
+        }
+      },
+      animation: {
+        'fade-in-out': 'fadeInOut 5s ease-in-out infinite'
+      },
+     
   },
   plugins: [],
 }
 
+}
